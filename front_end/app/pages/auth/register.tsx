7844ef5adfbf86fc14f116f645e01528e833838f
khanhtran0111 <giakhanhcnh@gmail.com>
2025-03-02 13:33:43 +0700

    adding login and regis

---
+++ front_end/app/pages/auth/register.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+import AuthLayout from '../../components/layouts/AuthLayout';
+import RegisterForm from '../../components/auth/RegisterForm';
+
+const RegisterPage: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Account | WealthMate</title>
+        <meta name="description" content="Create a new WealthMate account" />
+      </Head>
+      <AuthLayout title="Create Your Account">
+        <RegisterForm />
+      </AuthLayout>
+    </>
+  );
+};
+
+export default RegisterPage;
