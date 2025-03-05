8e2bebb29f6181ee0b11c93ab6f0e583b42b9ce1
VANH1810 <tqvabk24@gmail.com>
2025-03-06 00:42:06 +0700

    add transaction endpoint

--- back_end/src/app.module.ts
+++ back_end/src/app.module.ts
@@ -10,12 +10,11 @@ import { ReportsModule } from './reports/reports.module';
 import { GoalsModule } from './goals/goals.module';
 import { RecurringTransactionsModule } from './recurring-transactions/recurring-transactions.module';
 import { CategoriesModule } from './categories/categories.module';
-import { JarsModule } from './jars/jars.module';
 import { LoansModule } from './loans/loans.module';
 import { LoansController } from './loans/loans.controller';
 import { LoansService } from './loans/loans.service';
 import { AiRecommendationsModule } from './ai-recommendations/ai-recommendations.module';
-
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
@@ -28,9 +27,13 @@ import { AiRecommendationsModule } from './ai-recommendations/ai-recommendations
     GoalsModule,
     RecurringTransactionsModule,
     CategoriesModule,
-    JarsModule,
     LoansModule,
-    AiRecommendationsModule
+    AiRecommendationsModule,
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 300,
+      max: 100,
+    }),
   ],
   controllers: [AppController, LoansController],
   providers: [AppService, LoansService],
