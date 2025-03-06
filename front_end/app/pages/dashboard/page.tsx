bfefad3c749894295ab41b96de2ea1a7b88261c1
VanVu1306 <vunhattuongvan@gmail.com>
2025-03-06 15:37:33 +0700

    bug dashboard

--- front_end/app/pages/dashboard/page.tsx
+++ front_end/app/pages/dashboard/page.tsx
@@ -1,6 +1,13 @@
-const DashBoardPage = () => {
-    return <h1>Hello World</h1>;
-  };
-  
-  export default DashBoardPage;
-  
+import React from 'react';
+import MainLayout from '@/layouts/MainLayout';
+
+const DashBoardPage: React.FC = () => {
+  return (
+    <MainLayout>
+      <h1>Welcome to the Dashboard</h1>
+      <p>This is the content area.</p>
+    </MainLayout>
+  );
+};
+
+export default DashBoardPage;
