9550ce30554ce05caf103564da1333f35d61cbf7
VANH1810 <tqvabk24@gmail.com>
2025-03-08 00:35:13 +0700

    user service

--- back_end/src/GlobalVariables.ts
+++ back_end/src/GlobalVariables.ts
@@ -1,7 +1,6 @@
-import { User } from './users/entities/users.entity';
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class GlobalVariables {
-  public static currentUser: User;
+  
 }
