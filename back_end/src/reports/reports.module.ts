bde95b3809e44e0b4b3845e3ce014c0631d82a97
VANH1810 <tqvabk24@gmail.com>
2025-03-17 00:12:15 +0700

    debug transactions - budget

--- back_end/src/reports/reports.module.ts
+++ back_end/src/reports/reports.module.ts
@@ -6,11 +6,15 @@ import { CategoriesModule } from '../categories/categories.module';
 import { BudgetsModule } from '../budgets/budgets.module';
 import { UsersModule } from '../users/users.module';
 import { DateUtilsService } from '../common/services/date-utils.service';
+import { LoansModule } from 'src/loans/loans.module';
+import { GoalsModule } from 'src/goals/goals.module';
 @Module({
   imports: [
     TransactionsModule,
     CategoriesModule,
     BudgetsModule,
+    LoansModule,
+    GoalsModule,
     UsersModule,
   ],
   controllers: [ReportsController],
